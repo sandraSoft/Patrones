4ad4cf2694aeeaee1d3a25ff6b446ce79ef35a9e
Sandra Hurtado <sandra.hurtado@ucaldas.edu.co>
2023-03-15 11:25:53 -0500

    incluir pruebas de tarjeta

--- src/adapter/tests/test_operaciones_inicial.py
+++ src/adapter/tests/test_operaciones_inicial.py
@@ -30,3 +30,27 @@ class TestOperacionBancaria(unittest.TestCase):
 
         resultado_retiro = control_banco.realizar_retiro(cuenta, 700000)
         self.assertFalse(resultado_retiro)
+    
+
+    """
+    def test_retirar_tarjeta_normal(self):
+        control_banco = OperacionBancaria()
+        tarjeta = Tarjeta("123",800000)
+
+        resultado_retiro = control_banco.realizar_retiro(tarjeta, 600000)
+        self.assertTrue(resultado_retiro)
+    
+    def test_retirar_tarjeta_negativo(self):
+        control_banco = OperacionBancaria()
+        tarjeta = Tarjeta("456", 300000)
+
+        resultado_retiro = control_banco.realizar_retiro(tarjeta, -400000)
+        self.assertFalse(resultado_retiro)
+    
+    def test_retirar_tarjeta_mas_del_saldo(self):
+        control_banco = OperacionBancaria()
+        tarjeta = Tarjeta("789", 500000)
+
+        resultado_retiro = control_banco.realizar_retiro(tarjeta, 700000)
+        self.assertFalse(resultado_retiro)
+    """

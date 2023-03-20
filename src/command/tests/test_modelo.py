1c29b45315965030d7a8be30fe845c97fa2a0ac4
Sandra Hurtado <sandra.hurtado@ucaldas.edu.co>
2023-03-20 17:12:32 -0500

    Cambiar assert general por específicos

--- src/command/tests/test_modelo.py
+++ src/command/tests/test_modelo.py
@@ -2,26 +2,30 @@ import unittest
 from command.modelo import Apuesta
 
 class TestApuesta(unittest.TestCase):
-
-    DINERO_CERO = "Dinero = $0\n"
-    ACTIVA = "Estado = activa"
-    CANCELADA = "Estado = cancelada"
+    ACTIVA = "activa"
+    CANCELADA = "cancelada"
 
     def test_incrementar_apuesta(self):
         apuesta = Apuesta()
-        self.assertEqual(self.DINERO_CERO+self.ACTIVA,str(apuesta))
+        self.assertEqual(0,apuesta.cantidad_dinero)
+        self.assertEqual(self.ACTIVA,apuesta.estado)
         apuesta.incrementar()
-        self.assertEqual("Dinero = $10000\n"+self.ACTIVA,str(apuesta))
+        self.assertEqual(10000,apuesta.cantidad_dinero)
+        self.assertEqual(self.ACTIVA,apuesta.estado)
 
     def test_cancelar_apuesta(self):
         apuesta = Apuesta()
-        self.assertEqual(self.DINERO_CERO+self.ACTIVA,str(apuesta))
+        self.assertEqual(0,apuesta.cantidad_dinero)
+        self.assertEqual(self.ACTIVA,apuesta.estado)
         apuesta.cancelar()
-        self.assertEqual(self.DINERO_CERO+self.CANCELADA,str(apuesta))
+        self.assertEqual(0,apuesta.cantidad_dinero)
+        self.assertEqual(self.CANCELADA,apuesta.estado)
     
     def test_incrementar_apuesta_cancelada(self):
         apuesta = Apuesta()
-        self.assertEqual(self.DINERO_CERO+self.ACTIVA,str(apuesta))
+        self.assertEqual(0,apuesta.cantidad_dinero)
+        self.assertEqual(self.ACTIVA,apuesta.estado)
         apuesta.cancelar()
         apuesta.incrementar()
-        self.assertEqual(self.DINERO_CERO+self.CANCELADA,str(apuesta))
+        self.assertEqual(0,apuesta.cantidad_dinero)
+        self.assertEqual(self.CANCELADA,apuesta.estado)
